44fdb6b9b1ef693214d4244025559cf33d515630
kartikqh <139676238+kartikqh@users.noreply.github.com>
2023-11-24 06:57:33 +0000

    commit code

--- models/UserModel.js
+++ models/UserModel.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  age: { type: String, required: true },
+  age: { type: String},
   userType: { type: String, enum: ['Driver', 'Examiner', 'Admin'], required: true },
   firstname: { type: String, default: 'First Name' },
   lastname: { type: String, default: 'Last Name' },
